62432148df2413919598c6cc6609d6a7374b7f2f
Pblonghurst <59968417+Phil-Longhurst@users.noreply.github.com>
2025-06-29 17:52:58 +0100

    fix visual bug

--- components/HeaderLanding.vue
+++ components/HeaderLanding.vue
@@ -2,7 +2,7 @@
   <nav class="p-6 bg-gray-900 h-[100px] flex items-center">
     <div class="max-w-7xl mx-auto flex justify-between items-center w-full">
       <h1 class="text-2xl font-bold text-white">BoardGameHub</h1>
-      <div v-if="user" class="flex items-center gap-4">
+      <div v-if="user && !userLoading" class="flex items-center gap-4">
         <span class="text-white">Welcome, {{ user.email }}</span>
         <Button @click="authStore.handleSignOut('/')">Sign Out</Button>
       </div>
@@ -15,4 +15,5 @@ import { useAuthStore } from '@/store/authStore';
 const authStore = useAuthStore();
 
 const user = computed(() => authStore.user);
+const userLoading = computed(() => authStore.loading);
 </script>
